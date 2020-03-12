aa9c6ad092f941def42f86d00825009718f232df
chosh31 <sunghwancho2011@gmail.com>
2020-03-12 22:06:06 +0900

    update permissions

--- app/src/main/java/com/corona/maskmap/MainActivity.java
+++ app/src/main/java/com/corona/maskmap/MainActivity.java
@@ -1,25 +1,37 @@
 package com.corona.maskmap;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.webkit.GeolocationPermissions;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
-    private WebView mWebView; // 웹뷰 선언
     private WebSettings mWebSettings; //웹뷰세팅
+    private static final int MY_PERMISSION_REQUEST_LOCATION = 0;
+    private static final String WEB_VIEW_URL = "https://chosh31.github.io/mask-map/";
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.webView);
 
-        // 웹뷰 시작
-        mWebView = (WebView) findViewById(R.id.webView);
+        permissionCheck();
+    }
 
-        mWebView.setWebViewClient(new WebViewClient()); // 클릭시 새창 안뜨게
-        mWebSettings = mWebView.getSettings(); //세부 세팅 등록
+    private void initWebView(){
+        mWebSettings = webView.getSettings(); //세부 세팅 등록
         mWebSettings.setJavaScriptEnabled(true); // 웹페이지 자바스클비트 허용 여부
         mWebSettings.setSupportMultipleWindows(false); // 새창 띄우기 허용 여부
         mWebSettings.setJavaScriptCanOpenWindowsAutomatically(false); // 자바스크립트 새창 띄우기(멀티뷰) 허용 여부
@@ -30,7 +42,35 @@ public class MainActivity extends AppCompatActivity {
         mWebSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN); // 컨텐츠 사이즈 맞추기
         mWebSettings.setCacheMode(WebSettings.LOAD_NO_CACHE); // 브라우저 캐시 허용 여부
         mWebSettings.setDomStorageEnabled(true); // 로컬저장소 허용 여부
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+                super.onGeolocationPermissionsShowPrompt(origin, callback);
+                callback.invoke(origin, true, false);
+            }
+        });
 
-        mWebView.loadUrl("https://chosh31.github.io/mask-map/"); // 웹뷰에 표시할 웹사이트 주소, 웹뷰 시작
+
+        webView.loadUrl(WEB_VIEW_URL);
+    }
+
+    private void permissionCheck(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            //Manifest.permission.ACCESS_FINE_LOCATION 접근 승낙 상태 일때
+            initWebView();
+        } else{
+            //Manifest.permission.ACCESS_FINE_LOCATION 접근 거절 상태 일때
+            //사용자에게 접근권한 설정을 요구하는 다이얼로그를 띄운다.
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},MY_PERMISSION_REQUEST_LOCATION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == MY_PERMISSION_REQUEST_LOCATION){
+            initWebView();
+        }
     }
 }
